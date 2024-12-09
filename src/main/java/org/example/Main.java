b72d2bdf5fd7ac683f29936681669dac76898342
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2024-12-09 17:23:28 +0100

    rolled back to collections way

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,37 +1,29 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello in Refrigerator App");
-
-        // tablica (Array) Ingredient[] tablica
-
-        // kolekcja (Collection) List<Ingredient> kolekcja
 
         Ingredient milk = new Ingredient("milk", 500);
         Ingredient egg = new Ingredient("egg", 12);
 
 //        // here we create collection List of Ingredients
-//        List<Ingredient> ingredients = new ArrayList<>();
-        Ingredient[] ingredients = new Ingredient[2];
+        List<Ingredient> ingredients = new ArrayList<>();
 //        // here we add milk with 500 ml
-//        ingredients.add(milk);
-        ingredients[0] = milk;
+        ingredients.add(milk);
 //        // here we add 12 eggs
-//        ingredients.add(egg);
-        ingredients[1] = egg;
+        ingredients.add(egg);
 
 
         // here we create lodówka
         Refrigerator refrigerator = new Refrigerator(ingredients);
 
         // here we print ingredients from lodówka
-//        List<Ingredient> ingredientList = refrigerator.getIngredientList();
-        Ingredient[] ingredientsArray = refrigerator.getIngredientsArray();
-        for(int i = 0; i < ingredientsArray.length; i++){
-            System.out.println(ingredientsArray[i]);
-        }
-//        System.out.println(ingredientList);
+        List<Ingredient> ingredientList = refrigerator.getIngredientList();
+        System.out.println(ingredientList);
 
         System.out.println("Bye Bye");
     }
--- src/main/java/org/example/Refrigerator.java
+++ src/main/java/org/example/Refrigerator.java
@@ -1,45 +1,28 @@
 package org.example;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class Refrigerator {
-//    List<Ingredient> ingredientList;
-    Ingredient[] ingredients;
+    List<Ingredient> ingredientList;
 
-//    public Refrigerator(List<Ingredient> ingredientList) {
-//        this.ingredientList = ingredientList;
-//    }
-
-    public Refrigerator(Ingredient[] ingredients) {
-        this.ingredients = ingredients;
+    public Refrigerator(List<Ingredient> ingredientList) {
+        this.ingredientList = ingredientList;
     }
 
-//    public List<Ingredient> getIngredientList() {
-//        return ingredientList;
-//    }
-
-    public Ingredient[] getIngredientsArray() {
-        return ingredients;
+    public List<Ingredient> getIngredientList() {
+        return ingredientList;
     }
 
-//    public void setIngredientList(List<Ingredient> ingredientList) {
-//        this.ingredientList = ingredientList;
-//    }
+    public void setIngredientList(List<Ingredient> ingredientList) {
+        this.ingredientList = ingredientList;
+    }
 //     remove ingredient , add ingredient, check if is ingredient, = metody
-
-
-//    @Override
-//    public String toString() {
-//        return "Refrigerator{" +
-//                "ingredientList=" + ingredientList +
-//                '}';
-//    }
 
 
     @Override
     public String toString() {
         return "Refrigerator{" +
-                "ingredients=" + Arrays.toString(ingredients) +
+                "ingredientList=" + ingredientList +
                 '}';
     }
 }
