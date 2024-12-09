ed0ac68fbf8d1e3cf6280c71446ba88711d8d06f
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2024-12-09 18:01:12 +0100

    used for each

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,25 +3,27 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
+// klasa Main
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello in Refrigerator App");
 
         Ingredient milk = new Ingredient("milk", 500);
         Ingredient egg = new Ingredient("egg", 12);
+//        Ingredient oil = new Ingredient("oil", 100);
 
-//        // here we create collection List of Ingredients
         List<Ingredient> ingredients = new ArrayList<>();
-//        // here we add milk with 500 ml
         ingredients.add(milk);
-//        // here we add 12 eggs
         ingredients.add(egg);
+//        ingredients.add(oil);
 
-
-        // here we create lodówka
         Refrigerator refrigerator = new Refrigerator(ingredients);
 
-        // here we print ingredients from lodówka
+        // funkcja
+        refrigerator.isIngredientAvailable("oil");
+        refrigerator.isIngredientAvailable("milk");
+        refrigerator.isIngredientAvailable("egg");
+
         List<Ingredient> ingredientList = refrigerator.getIngredientList();
         System.out.println(ingredientList);
 
--- src/main/java/org/example/Refrigerator.java
+++ src/main/java/org/example/Refrigerator.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.List;
 
+// klasa Refrigerator
 public class Refrigerator {
     List<Ingredient> ingredientList;
 
@@ -18,11 +19,39 @@ public class Refrigerator {
     }
 //     remove ingredient , add ingredient, check if is ingredient, = metody
 
-
     @Override
     public String toString() {
         return "Refrigerator{" +
                 "ingredientList=" + ingredientList +
                 '}';
+    }
+
+    public void isIngredientAvailable(String ingredientName) {
+        System.out.println();
+        System.out.println("Stared function checking if " + ingredientName + " is available");
+//        String firstIngredientName = ingredientList.get(0).getName(); // milk
+//        String secondIngredientName = ingredientList.get(1).getName(); // egg
+//        String thirdIngredientName = ingredientList.get(2).getName(); // oil
+
+        for(Ingredient ingredient : ingredientList) {
+            System.out.println("checking: " + ingredient);
+            if (ingredient.getName().equals(ingredientName)) {
+                System.out.println("Yes! " + ingredientName + " is available");
+            } else {
+                System.out.println("No! " + ingredientName + " is NOT available");
+            }
+        }
+//
+//        if (firstIngredientName.equals(ingredientName)) {
+//            System.out.println("Yes! " + firstIngredientName + " is available");
+//        } else if (secondIngredientName.equals(ingredientName)) {
+//            System.out.println("Yes! " + secondIngredientName + " is available");
+//        } else if (thirdIngredientName.equals(ingredientName)) {
+//            System.out.println("Yes! " + thirdIngredientName + " is available");
+//        } else {
+//            System.out.println("No! " + ingredientName + " is NOT available");
+//        }
+        System.out.println("Ended function checking if " + ingredientName + " is available");
+        System.out.println();
     }
 }
